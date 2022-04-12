7c2130b17a4c4d166f82e99bd67a8bfeda194d7e
mohit sharma <mohitsharma.cse@gmail.com>
2022-04-12 18:31:38 -0500

    added margin to header

--- src/components/UI/PageTitle/PageTitle.js
+++ src/components/UI/PageTitle/PageTitle.js
@@ -5,7 +5,7 @@ import style from './pageTitle.module.css'
 function PageTitle(props) {
     return (
         <>
-            <h1 className="display-6 mb-0">
+            <h1 className="display-6 mb-0" style={{marginTop :'15px'}}>
                 <strong className={style.PageTitle}>{props.children}</strong>
             </h1>
             <div className={`mt-1 mb-4 ${style.HR}`} />
